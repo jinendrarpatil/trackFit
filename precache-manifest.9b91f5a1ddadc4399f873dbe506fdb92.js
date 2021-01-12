4b88f9b66cd61dfb7d778367c54ae36a367d9a58
jinendra patil <jinendrarpatil@gmail.com>
2021-01-12 14:41:53 -0500

    Updates

---
+++ precache-manifest.9b91f5a1ddadc4399f873dbe506fdb92.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6a2e2f7682d65d2077c14f9ad187419",
+    "url": "/trackFit/index.html"
+  },
+  {
+    "revision": "d2795bc44940261af050",
+    "url": "/trackFit/static/css/2.76dbdff4.chunk.css"
+  },
+  {
+    "revision": "8c450c0a9dd2b2ca8c44",
+    "url": "/trackFit/static/css/main.21ff196e.chunk.css"
+  },
+  {
+    "revision": "d2795bc44940261af050",
+    "url": "/trackFit/static/js/2.2ca05272.chunk.js"
+  },
+  {
+    "revision": "8c450c0a9dd2b2ca8c44",
+    "url": "/trackFit/static/js/main.acee1cb8.chunk.js"
+  },
+  {
+    "revision": "5a853eb45dfe10e36655",
+    "url": "/trackFit/static/js/runtime~main.19ede153.js"
+  },
+  {
+    "revision": "2ed23e89c029417b67c9bc7218130ed1",
+    "url": "/trackFit/static/media/1.2ed23e89.jpg"
+  },
+  {
+    "revision": "9816584563d052de97a19b9ae2e92eaa",
+    "url": "/trackFit/static/media/2.98165845.jpg"
+  },
+  {
+    "revision": "28e0e3990b212f85bd8b36d850580223",
+    "url": "/trackFit/static/media/3.28e0e399.jpg"
+  },
+  {
+    "revision": "aae92f175721d388712c71f4d2976694",
+    "url": "/trackFit/static/media/4.aae92f17.jpg"
+  },
+  {
+    "revision": "05c6877cc66ff26525102fe31325f368",
+    "url": "/trackFit/static/media/5.05c6877c.jpg"
+  }
+]);
